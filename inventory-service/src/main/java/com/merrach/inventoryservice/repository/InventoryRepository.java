e0372843f23113c3107d431cf4dae1c046df29c6
SamMrach <sammerche84@gmail.com>
2024-01-05 17:30:35 +0100

    create inventory service

---
+++ inventory-service/src/main/java/com/merrach/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,11 @@
+package com.merrach.inventoryservice.repository;
+
+import com.merrach.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+
+}
