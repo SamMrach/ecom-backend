8aeccc9db98247e3c313bbfc1a279bd80b583844
SamMrach <sammerche84@gmail.com>
2024-01-10 10:44:41 +0100

    add circuit breaker

--- order-service/src/main/java/com/merrach/orderservice/Config/WebClientConfig.java
+++ order-service/src/main/java/com/merrach/orderservice/Config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.merrach.orderservice.Config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,6 +10,7 @@ public class WebClientConfig {
 
 
     @Bean
+    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
